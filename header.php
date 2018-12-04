73b84e8c07197161e26e3a3f10768b1a699668b3
Tyler Jackson <tljacks5@gmail.com>
2018-12-04 10:11:02 -0600

    adding some basic theme files for a faster start to theming

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+
+    <?php wp_head(); ?>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/scripts/dist/application.js"></script>
+
+    <link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" />
+
+    <!--[if IE]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+    </header>
--- index.php
+++ index.php
@@ -1,40 +1,8 @@
-<!doctype html>
-<html class="no-js" lang="">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-
-		<title>Website</title>
-
-		<link href="assets/stylesheets/css/application.css" rel="stylesheet" />
-		<script src="assets/javascripts/jquery.js"></script>
-		<script src="assets/javascripts/modernizr.js"></script>
-		<!--[if IE]>
-    	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-	</head>
-
-	<body>
-		<!--[if lte IE 8]>
-			<div class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</div>
-		<![endif]-->
-		<header>
-			<div class="container">
-			</div>
-		</header>
-
+<?php get_header(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 		<main>
-			<section>
-				<div class="container">
-				</div>
-			</section>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
 		</main>
-
-		<footer>
-			<div class="container">
-			</div>
-		</footer>
-	</body>
-</html>
+	<?php endwhile; ?>
+<?php get_footer(); ?>
