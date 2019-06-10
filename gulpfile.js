c6b62fef47b3e7a96a46bb45095e61f3600810e3
Tyler Jackson <tljacks5@gmail.com>
2019-06-10 12:31:42 -0500

    adding livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     plumber = require('gulp-plumber'),
     sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat')
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload');
 
 //Set config variables
 var config = {
@@ -23,21 +23,12 @@ function styles() {
     .src(config.sassFiles)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true,
-      outputStyle: 'compressed',
-      //outputStyle: 'compact',
-      //outputStyle: 'nested',
-      //outputStyle: 'expanded',
-      precision: 10
-    }))
+    .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write({includeContent: false}))
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('.'))
-    .pipe(plumber.stop())
-    //Set destination to CSS directory
     .pipe(gulp.dest(config.cssPath))
+    .pipe(livereload())
 }
 
 //Compile the scripts
@@ -54,6 +45,7 @@ function scripts() {
 
 //Set which files to watch
 function watch() {
+  livereload.listen();
   gulp.watch(config.sassFiles, styles);
   gulp.watch(config.jsFiles, scripts);
 }
